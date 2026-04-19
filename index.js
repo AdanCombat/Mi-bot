489ed9b0dab48fc59cbf606367b84bd830128eb1
AdanCombat <151210623+AdanCombat@users.noreply.github.com>
2026-04-19 01:52:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ client.on('ready', () => {
 client.on('messageCreate', message => {
     if (message.author.bot) return;
 
-    if (message.content === "!agarraralagordadestarpmientrasmecomounabigmacuwu") {
+    if (message.content === "!agarroalagordadestarpmientrasmecomounabigmacuwu") {
         message.channel.send("https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExdThxbGZsaGtsM2Y5Y2Znemx4MDI5Yzd1NDRsOWJvZDVlY2l4MjdsMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Qmo9T5uI089dkyz3Bd/giphy.gif");
     }
 });
